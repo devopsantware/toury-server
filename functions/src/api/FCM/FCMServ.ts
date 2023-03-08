a5019e6a3e485527aa4a191f18badb65a13ecc00
Irving Guerra <guerravargasirving@gmail.com>
2023-03-07 20:58:53 -0600

    Feature: Fix doc path

--- functions/src/api/FCM/FCMServ.ts
+++ functions/src/api/FCM/FCMServ.ts
@@ -3,7 +3,7 @@ import { usersRef, UserType } from '../Users/User';
 export default {
     async getToken(body: any): Promise<string | undefined> {
         if (body.userId) {
-            const userDoc = await usersRef.doc(body.customerId).get();
+            const userDoc = await usersRef.doc(body.userId).get();
             const customer = userDoc.data() as UserType;
             return customer.deviceToken;
         }
